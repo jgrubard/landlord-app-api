408923022251e21700bc8c77ad4f36f211fcf73a
Jeremy Grubard <jgrubard@gmail.com>
2019-03-05 18:52:58 -0500

    remove validations

--- app/models/tenant_application.rb
+++ app/models/tenant_application.rb
@@ -1,11 +1,11 @@
 class TenantApplication < ApplicationRecord
 
-  validates :application_type, presence: true
-  validates :token, uniqueness: true
-  validates :email, uniqueness: true
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :update }
-  validates :landlord_phone, length: { is: 10 }
-  validates :phone, length: { is: 10 }
-  validates :ssn,  length: { is: 9 }
+  # validates :application_type, presence: true
+  # validates :token, uniqueness: true
+  # validates :email, uniqueness: true
+  # validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :update }
+  # validates :landlord_phone, length: { is: 10 }
+  # validates :phone, length: { is: 10 }
+  # validates :ssn,  length: { is: 9 }
 
 end
